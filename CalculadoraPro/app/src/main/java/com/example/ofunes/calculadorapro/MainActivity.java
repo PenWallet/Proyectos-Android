cbc0d2359fa20e0593aa9a2725d13fbd1f6f2b68
Unknown <carterasindinero@gmail.com>
2018-10-18 10:27:13 +0200

    APK hecha y comentarios añadidos

--- CalculadoraPro/app/src/main/java/com/example/ofunes/calculadorapro/MainActivity.java
+++ CalculadoraPro/app/src/main/java/com/example/ofunes/calculadorapro/MainActivity.java
@@ -55,17 +55,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch(btnID)
         {
+            //En caso de que se haya pulsado un botón de número o el punto
             case R.id.btn0: case R.id.btn1: case R.id.btn2: case R.id.btn3: case R.id.btn4: case R.id.btn5: case R.id.btn6: case R.id.btn7: case R.id.btn8: case R.id.btn9: case R.id.btnDot:
+                //Si el resultado ya está en pantalla
                 if(resultadoEnPantalla)
                 {
+                    //Se limpia lo que haya en ambos textBoxes, se cambia el botón a <- y se actualiza la variable
                     resetearTxtNumbers();
                     resetearTxtResults();
                     bDelete.setText(res.getString(R.string.delete));
                     resultadoEnPantalla = false;
                 }
 
+                //Dependiendo de qué botón numérico o el punto haya pulsado
                 switch(btnID)
                 {
+                    //Se escribe en txtNumbers el número o punto pulsado
                     case R.id.btn0: escribirNumero(0); break;
                     case R.id.btn1: escribirNumero(1); break;
                     case R.id.btn2: escribirNumero(2); break;
@@ -85,60 +90,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             break;
 
+            //Si se ha pulsado un botón de operación
             case R.id.btnAddition: case R.id.btnSubstraction: case R.id.btnDivision: case R.id.btnMultiplication:
-                //Para que el botón haga algo, no debe estar el resultado en pantalla ni debe haber
-                //nada en la parte de arriba de las operaciones
+                //Para que el botón reaccione, eliminamos la posibilidad de que haya únicamente símbolos no numéricos en txtNumbers
                 if(!txtNumbers.getText().toString().equals("-") && !txtNumbers.getText().toString().equals("-.") && !txtNumbers.getText().toString().equals("."))
                 {
+                    //Para que el botón haga algo, no debe estar el resultado en pantalla ni debe haber
+                    //nada en la parte de arriba de las operaciones
                     if (!resultadoEnPantalla && txtResults.getText().toString().equals(""))
                     {
-                        if (txtNumbers.getText().toString().equals(""))
-                        {
-                            txtResults.setText("0");
-                            n1 = 0;
-                        }
-                        else
-                        {
-                            n1 = Double.parseDouble(txtNumbers.getText().toString());
-                            txtResults.setText(txtNumbers.getText());
-                        }
+                        //Se toma como n1 lo que haya escrito en txtNumbers y se coloca en txtResults
+                        n1 = Double.parseDouble(txtNumbers.getText().toString());
+                        txtResults.setText(txtNumbers.getText());
 
+                        //Se resetea a 0 el texto que haya en txtNumbers
                         resetearTxtNumbers();
 
+                        //Dependiendo de qué botón de operación haya pulsado
                         switch (btnID)
                         {
+                            //Se toma en una variable qué operador ha sido pulsado
                             case R.id.btnAddition: operador = '+'; break;
                             case R.id.btnSubstraction: operador = '-'; break;
                             case R.id.btnDivision: operador = '/'; break;
                             case R.id.btnMultiplication: operador = '*'; break;
                         }
 
+                        //Y se coloca junto al texto (que es n1) que había previamente en txtResults
                         txtResults.setText(txtResults.getText() + " " + operador + " ");
                     }
-                    else if (resultadoEnPantalla)
+                    else if (resultadoEnPantalla) //En caso de que el resultado ya esté en pantalla
                     {
+                        /* Se resetea la variable de resultadoEnPantalla ya que se van a realizar
+                           operaciones sobre el resultado anterior
+                         */
                         resultadoEnPantalla = false;
                         bDelete.setText(res.getString(R.string.delete));
                         txtResults.setText(txtNumbers.getText());
                         n1 = resultado;
                         resultado = 0;
 
+                        //Se resete txtNumbers a 0
                         resetearTxtNumbers();
 
+                        //Dependiendo de qué botón de operación haya pulsado
                         switch (btnID)
                         {
+                            //Se toma en una variable qué operador ha sido pulsado
                             case R.id.btnAddition: operador = '+'; break;
                             case R.id.btnSubstraction: operador = '-'; break;
                             case R.id.btnDivision: operador = '/'; break;
                             case R.id.btnMultiplication: operador = '*'; break;
                         }
 
+                        //Y se coloca junto al texto (que es n1) que había previamente en txtResults
                         txtResults.setText(txtResults.getText() + " " + operador + " ");
                     }
-                    else if (operador != '0' && !resultadoEnPantalla)
+                    else if (operador != '0' && !resultadoEnPantalla) //Si ya se ha pulsado un operador pero no se ha pulsado el igual
                     {
+                        //Se toma como n2 lo que haya en txtNumbers escrito
                         n2 = Double.parseDouble(txtNumbers.getText().toString());
 
+                        //Se hace la operación pendiente con el operador que ya hubiese en la variable
                         switch (operador)
                         {
                             case '+': n1 = n1 + n2; break;
@@ -147,6 +160,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             case '/': n1 = n1 / n2; break;
                         }
 
+                        //Y se cambia al operador que se haya pulsado
                         switch (btnID)
                         {
                             case R.id.btnAddition: operador = '+'; break;
@@ -155,16 +169,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             case R.id.btnMultiplication: operador = '*'; break;
                         }
 
+                        //Finalmente se coloca en txtResults
                         txtResults.setText(String.valueOf(n1) + " " + operador + " ");
 
+                        //Y se resetea txtNumbers a 0
                         resetearTxtNumbers();
                     }
                 }
             break;
 
+            //En caso de haber pulsado el botón de borrar
             case R.id.btnDelete:
+                //En caso de que el resultado esté en pantalla
                 if(resultadoEnPantalla)
                 {
+                    /*El botón tendrá como texto 'C', por lo que se resetea a '<-', se resetean los
+                    TextBoxes y las variables */
                     bDelete.setText(res.getString(R.string.delete));
                     resultadoEnPantalla = false;
                     resetearTxtNumbers();
@@ -173,13 +193,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     n2 = 0;
                     operador = '0';
                 }
-                else
+                else //Si el resultado no está en pantalla
                 {
+                    //Si txtNumbers solo tiene una longitud de 1, entonces se resetea txtNumbers a 0
                     if(txtNumbers.getText().toString().length() == 1)
                     {
                         resetearTxtNumbers();
                     }
-                    else
+                    else //Si no mide solo 1
                     {
                         //Se le quita el último número del string
                         txtNumbers.setText(txtNumbers.getText().toString().substring(0, txtNumbers.getText().toString().length() - 1));
@@ -188,22 +209,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 break;
 
+            //En caso de pulsar el botón de igual
             case R.id.btnEquals:
+                //Se comprueba que en txtResults haya valores numéricos, si no, no entra y no hace nada
                 if(!txtResults.getText().toString().equals("") && !txtNumbers.getText().toString().equals("-") && !txtNumbers.getText().toString().equals("."))
                 {
+                    //En caso de que el resultado ya esté en pantalla
                     if(resultadoEnPantalla)
                     {
+                        //Se toma como n1 el resultado anterior y se coloca en txtResults junto al operador
                         n1 = resultado;
                         txtResults.setText(String.valueOf(n1)+" "+operador+" "+n2);
                     }
-                    else
+                    else //Si el resultado no está en pantalla
                     {
+                        /*Se actualiza la variable correspondiente, se cambia el botón de borrar
+                        de '<-' a 'C', se toma como n2 el texto que haya en txtNumbers, y se coloca
+                        el texto que haya en txtNumbers tras lo que ya haya en txtResults
+                         */
                         resultadoEnPantalla = true;
                         bDelete.setText(res.getString(R.string.deleteAll));
                         n2 = Double.parseDouble(txtNumbers.getText().toString());
                         txtResults.setText(txtResults.getText()+txtNumbers.getText().toString());
                     }
 
+                    //Dependiendo de qué operador se eligiese, se hace la operación correspondiente
                     switch(operador)
                     {
                         case '+': resultado = n1 + n2; break;
@@ -211,21 +241,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         case '*': resultado = n1 * n2; break;
                         case '/': resultado = n1 / n2; break;
                     }
+                    //Y se coloca en txtNumbers el resulado
                     txtNumbers.setText(String.valueOf(resultado));
                 }
                 break;
 
+            //En caso de pulsar el botón de '+/-'
             case R.id.btnPlusMinus:
+                //En caso de que lo único que haya en txtNumbers sea un 0
                 if(txtNumbers.getText().toString().equals("0"))
-                    txtNumbers.setText("-");
+                    txtNumbers.setText("-"); //Se cambia el texto por un '-'
+                //Si no, si el primer carácter de la cadena de txtNumbers no es '-'
                 else if(txtNumbers.getText().toString().toCharArray()[0] != '-')
-                    txtNumbers.setText("-"+txtNumbers.getText());
+                    txtNumbers.setText("-"+txtNumbers.getText()); //Se le añade al principio
+                //Si no, si lo único que hay es '-'
                 else if(txtNumbers.getText().toString().equals("-"))
-                    resetearTxtNumbers();
+                    resetearTxtNumbers(); //Se resetea txtNumbers a '0'
+                //Si no, entonces es que tiene '-' al principio del string
                 else
-                    txtNumbers.setText(txtNumbers.getText().toString().substring(1));
-
-
+                    txtNumbers.setText(txtNumbers.getText().toString().substring(1)); //Se elimina el primer carácter de la cadena
                 break;
         }
     }
@@ -239,7 +273,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * Procedimiento que limpia el textBox de los números
+     * Procedimiento que limpia el textBox de los números y pone un 0
      */
     private void resetearTxtNumbers()
     {
