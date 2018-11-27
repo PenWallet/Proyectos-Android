ba73a2ffb263f7d0a509b733b0d448a4cda54f53
Unknown <carterasindinero@gmail.com>
2018-11-27 10:06:49 +0100

    Jiji

--- ejemplorest/model/LibroHandlerModel.php
+++ ejemplorest/model/LibroHandlerModel.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "ConsLibrosModel.php";
-
+require_once "DatabaseModel.php";
 
 class LibroHandlerModel
 {
@@ -71,6 +71,49 @@ class LibroHandlerModel
         $db_connection->close();
 
         return $listaLibros;
+    }
+
+    public static function insertLibro($libro)
+    {
+        $db = DatabaseModel::getInstance();
+        $db_connection = $db->getConnection();
+        $insertado = false;
+
+
+        //IMPORTANT: we have to be very careful about automatic data type conversions in MySQL.
+
+        //If the $id is valid or the client asks for the collection ($id is null)
+        if ($libro instanceof LibroModel)
+        {
+            $query = "INSERT INTO libros (".\ConstantesDB\ConsLibrosModel::TITULO.", ".\ConstantesDB\ConsLibrosModel::PAGS.") VALUES (?. ?)";
+
+            $prep_query = $db_connection->prepare($query);
+
+            //IMPORTANT: If we do not want to expose our primary keys in the URIS,
+            //we can use a function to transform them.
+            //For example, we can use hash_hmac:
+            //http://php.net/manual/es/function.hash-hmac.php
+            //In this example we expose primary keys considering pedagogical reasons
+
+            $prep_query->bind_param('s', $libro->getTitulo());
+            $prep_query->bind_param('i', $libro->getNumpag());
+
+            $prep_query->execute();
+
+            //IMPORTANT: IN OUR SERVER, I COULD NOT USE EITHER GET_RESULT OR FETCH_OBJECT,
+            // PHP VERSION WAS OK (5.4), AND MYSQLI INSTALLED.
+            // PROBABLY THE PROBLEM IS THAT MYSQLND DRIVER IS NEEDED AND WAS NOT AVAILABLE IN THE SERVER:
+            // http://stackoverflow.com/questions/10466530/mysqli-prepared-statement-unable-to-get-result
+
+            $filasAfectadas = $prep_query->affected_rows;
+
+            if($filasAfectadas == 1)
+                $insertado = true;
+
+        }
+        $db_connection->close();
+
+        return $insertado;
     }
 
     //returns true if $id is a valid id for a book
