c7d52626c9f48071a1332dfdcd8616d8bf8d0525
Unknown <carterasindinero@gmail.com>
2018-11-28 10:17:57 +0100

    w

--- ejemplorest/controller/LibroController.php
+++ ejemplorest/controller/LibroController.php
@@ -40,9 +40,51 @@ class LibroController extends Controller
 
     }
 
+    public function manageDeleteVerb(Request $request)
+    {
+        $id = null;
+        $response = null;
+        $code = null;
+
+        //if the URI refers to a libro entity, instead of the libro collection
+        if (isset($request->getUrlElements()[2])) {
+            $id = $request->getUrlElements()[2];
+        }
+
+        $filasAfectadas = LibroHandlerModel::deleteLibro($id);
+
+        if ($filasAfectadas)
+            $code = '200';
+        else
+        {
+            //We could send 404 in any case, but if we want more precission,
+            //we can send 400 if the syntax of the entity was incorrect...
+            if (LibroHandlerModel::isValid($id))
+                $code = '404';
+            else
+                $code = '400';
+        }
+
+        $response = new Response($code, null, null, $request->getAccept());
+        $response->generate();
+    }
+
     public function managePostVerb(Request $request)
     {
+        $libro = (object)$request->getBodyParameters();
 
+        $response = null;
+        $code = null;
+
+        $filasAfectadas = LibroHandlerModel::insertLibro($libro);
+
+        if ($filasAfectadas)
+            $code = '200';
+        else
+            $code = '400';
+
+        $response = new Response($code, null, null, $request->getAccept());
+        $response->generate();
     }
 
 }
--- ejemplorest/model/LibroHandlerModel.php
+++ ejemplorest/model/LibroHandlerModel.php
@@ -73,19 +73,20 @@ class LibroHandlerModel
         return $listaLibros;
     }
 
-    public static function insertLibro($libro)
+    public static function deleteLibro($id)
     {
         $db = DatabaseModel::getInstance();
         $db_connection = $db->getConnection();
-        $insertado = false;
 
 
         //IMPORTANT: we have to be very careful about automatic data type conversions in MySQL.
 
+        $valid = self::isValid($id);
+
         //If the $id is valid or the client asks for the collection ($id is null)
-        if ($libro instanceof LibroModel)
+        if ($valid === true && $id != null)
         {
-            $query = "INSERT INTO libros (".\ConstantesDB\ConsLibrosModel::TITULO.", ".\ConstantesDB\ConsLibrosModel::PAGS.") VALUES (?. ?)";
+            $query = "DELETE FROM ".\ConstantesDB\ConsLibrosModel::TABLE_NAME." WHERE ".\ConstantesDB\ConsLibrosModel::COD." = ?";
 
             $prep_query = $db_connection->prepare($query);
 
@@ -95,22 +96,57 @@ class LibroHandlerModel
             //http://php.net/manual/es/function.hash-hmac.php
             //In this example we expose primary keys considering pedagogical reasons
 
-            $prep_query->bind_param('s', $libro->getTitulo());
-            $prep_query->bind_param('i', $libro->getNumpag());
+            if ($id != null)
+            {
+                $prep_query->bind_param('s', $id);
+            }
 
-            $prep_query->execute();
+            $filasAfectadas = $prep_query->execute();
 
-            //IMPORTANT: IN OUR SERVER, I COULD NOT USE EITHER GET_RESULT OR FETCH_OBJECT,
+            // IMPORTANT: IN OUR SERVER, I COULD NOT USE EITHER GET_RESULT OR FETCH_OBJECT,
             // PHP VERSION WAS OK (5.4), AND MYSQLI INSTALLED.
             // PROBABLY THE PROBLEM IS THAT MYSQLND DRIVER IS NEEDED AND WAS NOT AVAILABLE IN THE SERVER:
             // http://stackoverflow.com/questions/10466530/mysqli-prepared-statement-unable-to-get-result
 
-            $filasAfectadas = $prep_query->affected_rows;
+             //$prep_query->affected_rows;
 
             if($filasAfectadas == 1)
-                $insertado = true;
+                $ret = true;
+            else
+                $ret = false;
 
         }
+        $db_connection->close();
+
+        return $ret;
+    }
+
+    public static function insertLibro($libro)
+    {
+        $db = DatabaseModel::getInstance();
+        $db_connection = $db->getConnection();
+        $insertado = false;
+        $filasAfectadas = 0;
+
+        $query = "INSERT INTO libros (titulo, numpag) VALUES (?,?)";
+
+        $prep_query = $db_connection->prepare($query);
+
+        //IMPORTANT: If we do not want to expose our primary keys in the URIS,
+        //we can use a function to transform them.
+        //For example, we can use hash_hmac:
+        //http://php.net/manual/es/function.hash-hmac.php
+        //In this example we expose primary keys considering pedagogical reasons
+
+        $prep_query->bind_param('si', $libro->titulo, $libro->numpag);
+
+        $filasAfectadas = $prep_query->execute();
+
+        if($filasAfectadas == 1)
+            $insertado = true;
+        else
+            $insertado = false;
+
         $db_connection->close();
 
         return $insertado;
