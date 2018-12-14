b8dfa79c7a275d15647cc1bd23201360df467500
Unknown <carterasindinero@gmail.com>
2018-12-14 14:44:33 +0100

    Jejeje que me iba

--- PennyPanPhone/app/src/main/java/com/example/ofunes/pennypanphone/MainActivity.java
+++ PennyPanPhone/app/src/main/java/com/example/ofunes/pennypanphone/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.ofunes.pennypanphone;
 
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
